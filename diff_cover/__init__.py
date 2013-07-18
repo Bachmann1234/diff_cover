a7f7de7a83f00cb9e082a208c6f3ab58a451a639
Sarina Canelake <sarina@mitx.mit.edu>
2013-07-18 14:12:25 -0400

    Upgrade version

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.1.4'
+VERSION = '0.2.0'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
