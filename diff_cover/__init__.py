d6aad001cc31f80f9dcaac0e33b9a9a2f7ac45d5
Will Daly <will.e.daly@gmail.com>
2013-09-01 11:34:51 -0400

    Incremented to v0.2.2, updated CHANGELOG

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
