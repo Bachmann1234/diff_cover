7e0f098ec00423ac1293f1b1c9efc5281da50718
Will Daly <will@edx.org>
2013-09-23 13:30:36 -0400

    Updated version to 0.2.4

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
