a03b74b25c80aa64964e2f2c7a882fe0fc302d20
Matt Bachmann <bachmann.matt@gmail.com>
2016-02-09 21:12:46 -0500

    Version 0.9.3

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.9.2'
+VERSION = '0.9.3'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
