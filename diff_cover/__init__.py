d6dbf918b97b5e929c9e7c84015e3ff40e2bad7d
Matt Bachmann <Bachmann1234@users.noreply.github.com>
2019-03-16 12:19:19 -0400

    Version bump

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '1.0.6'
+VERSION = '1.0.7'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
