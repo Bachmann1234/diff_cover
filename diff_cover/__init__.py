6eebee9fe51148f34b87126ec71134e6b4b9942e
Matt Bachmann <bachmann.matt@gmail.com>
2019-10-17 21:11:06 -0400

    Version 2.4.0

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
-VERSION = '2.3.0'
+VERSION = '2.4.0'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
