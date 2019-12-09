df6aeeb877ce278d2a6bafdbbdfbcea8c5ade5b6
Barry Hart <barrywhart@yahoo.com>
2019-12-09 08:53:27 -0500

    Issue 8: Add a plugin system for quality reporters

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 
+import pluggy
+
 VERSION = '2.4.1'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
+
+# Other packages that implement diff_cover plugins use this.
+hookimpl = pluggy.HookimplMarker('diff_cover')
