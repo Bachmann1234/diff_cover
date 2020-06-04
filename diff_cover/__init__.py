83f00c883088bd871965a7145cc0550ca0db1dd3
Matt Bachmann <bachmann.matt@gmail.com>
2020-06-04 17:16:48 -0400

    Version 3.0.1

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -2,6 +2,6 @@ from __future__ import unicode_literals
 
 # This file is imported in setup.py and cannot use any external dependencies
 
-VERSION = '3.0.0'
+VERSION = '3.0.1'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
