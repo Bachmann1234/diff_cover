4669c71c7c14628fcdeefb20f57e3472afb8af0a
Kai Mueller <15907922+kasium@users.noreply.github.com>
2021-07-08 06:49:43 +0000

    Migrate test_git_path to native pytest

--- tests/test_git_path.py
+++ tests/test_git_path.py
@@ -1,81 +1,97 @@
-import unittest
-from unittest import mock
+# pylint: disable=missing-function-docstring
+
+"""Test for diff_cover.git_path"""
+
+import pytest
 
 from diff_cover.git_path import GitPathTool
 
 
-class TestGitPathTool(unittest.TestCase):
-    def setUp(self):
-        # Create mock subprocess to simulate `git rev-parse`
-        self.process = mock.Mock()
-        self.process.returncode = 0
-        self.subprocess = mock.patch("diff_cover.command_runner.subprocess").start()
-        self.subprocess.Popen.return_value = self.process
-        self.addCleanup(mock.patch.stopall)
+@pytest.fixture(autouse=True)
+def path_git_path_tool(mocker):
+    mocker.patch.object(GitPathTool, "_root", None)
+    mocker.patch.object(GitPathTool, "_cwd", None)
 
-    def tearDown(self):
-        # Reset static class members
-        GitPathTool._root = None
-        GitPathTool._cwd = None
 
-    def test_project_root_command(self):
-        self._set_git_root(b"/phony/path")
+@pytest.fixture
+def process(mocker):
+    process_ = mocker.Mock()
+    process_.returncode = 0
+    return process_
 
-        GitPathTool.set_cwd(b"/phony/path")
 
-        # Expect that the correct command was executed
-        expected = ["git", "rev-parse", "--show-toplevel", "--encoding=utf-8"]
-        self.subprocess.Popen.assert_called_with(
-            expected, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE
-        )
+@pytest.fixture
+def subprocess(mocker, process):
+    subprocess_ = mocker.patch("diff_cover.command_runner.subprocess")
+    subprocess_.Popen.return_value = process
+    return subprocess_
 
-    def test_relative_path(self):
-        self._set_git_root(b"/home/user/work/diff-cover")
-        expected = "violations_reporter.py"
-        cwd = "/home/user/work/diff-cover/diff_cover"
 
-        GitPathTool.set_cwd(cwd)
-        path = GitPathTool.relative_path("diff_cover/violations_reporter.py")
+def test_project_root_command(process, subprocess):
+    process.communicate.return_value = (b"/phony/path", b"")
 
-        # Expect relative path from diff_cover
-        self.assertEqual(path, expected)
+    GitPathTool.set_cwd(b"/phony/path")
 
-    def test_absolute_path(self):
-        self._set_git_root(b"/home/user/work dir/diff-cover\n--encoding=utf-8\n")
-        expected = "/home/user/work dir/diff-cover/other_package/file.py"
-        cwd = "/home/user/work dir/diff-cover/diff_cover"
+    # Expect that the correct command was executed
+    expected = ["git", "rev-parse", "--show-toplevel", "--encoding=utf-8"]
+    subprocess.Popen.assert_called_with(
+        expected, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
-        GitPathTool.set_cwd(cwd)
-        path = GitPathTool.absolute_path("other_package/file.py")
 
-        # Expect absolute path to file.py
-        self.assertEqual(path, expected)
+def test_relative_path(process):
+    process.communicate.return_value = (b"/home/user/work/diff-cover", b"")
 
-    def test_set_cwd_unicode(self):
-        self._set_git_root(b"\xe2\x94\xbb\xe2\x94\x81\xe2\x94\xbb")
-        expected = "\u253b\u2501\u253b/other_package/file.py"
-        cwd = "\\u253b\\u2501\\u253b/diff_cover\n--encoding=utf-8\n"
+    expected = "violations_reporter.py"
+    cwd = "/home/user/work/diff-cover/diff_cover"
 
-        GitPathTool.set_cwd(cwd)
-        path = GitPathTool.absolute_path("other_package/file.py")
+    GitPathTool.set_cwd(cwd)
+    path = GitPathTool.relative_path("diff_cover/violations_reporter.py")
 
-        # Expect absolute path to file.py
-        self.assertEqual(path, expected)
+    # Expect relative path from diff_cover
+    assert path == expected
 
-    def test_set_cwd_unicode_byte_passed_in_for_cwd(self):
-        self._set_git_root(b"\xe2\x94\xbb\xe2\x94\x81\xe2\x94\xbb\n--encoding=utf-8\n")
-        expected = "\u253b\u2501\u253b/other_package/file.py"
-        cwd = b"\\u253b\\u2501\\u253b/diff_cover"
 
-        GitPathTool.set_cwd(cwd)
-        path = GitPathTool.absolute_path("other_package/file.py")
+def test_absolute_path(process):
+    process.communicate.return_value = (
+        b"/home/user/work dir/diff-cover\n--encoding=utf-8\n",
+        b"",
+    )
 
-        # Expect absolute path to file.py
-        self.assertEqual(path, expected)
+    expected = "/home/user/work dir/diff-cover/other_package/file.py"
+    cwd = "/home/user/work dir/diff-cover/diff_cover"
 
-    def _set_git_root(self, git_root):
-        """
-        Configure the process mock to output `stdout`
-        to a given git project root.
-        """
-        self.process.communicate.return_value = (git_root, b"")
+    GitPathTool.set_cwd(cwd)
+    path = GitPathTool.absolute_path("other_package/file.py")
+
+    # Expect absolute path to file.py
+    assert path == expected
+
+
+def test_set_cwd_unicode(process):
+    process.communicate.return_value = (b"\xe2\x94\xbb\xe2\x94\x81\xe2\x94\xbb", b"")
+
+    expected = "\u253b\u2501\u253b/other_package/file.py"
+    cwd = "\\u253b\\u2501\\u253b/diff_cover\n--encoding=utf-8\n"
+
+    GitPathTool.set_cwd(cwd)
+    path = GitPathTool.absolute_path("other_package/file.py")
+
+    # Expect absolute path to file.py
+    assert path == expected
+
+
+def test_set_cwd_unicode_byte_passed_in_for_cwd(process):
+    process.communicate.return_value = (
+        b"\xe2\x94\xbb\xe2\x94\x81\xe2\x94\xbb\n--encoding=utf-8\n",
+        b"",
+    )
+
+    expected = "\u253b\u2501\u253b/other_package/file.py"
+    cwd = b"\\u253b\\u2501\\u253b/diff_cover"
+
+    GitPathTool.set_cwd(cwd)
+    path = GitPathTool.absolute_path("other_package/file.py")
+
+    # Expect absolute path to file.py
+    assert path == expected
