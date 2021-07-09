79b899e30f63cdc78acf83f57144385383aeb7ac
Matt Bachmann <bachmann.matt@gmail.com>
2021-07-09 16:47:27 -0400

    Version 6.1.1

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,5 +1,5 @@
 # This file is imported in setup.py and cannot use any external dependencies
 
-VERSION = "6.1.0"
+VERSION = "6.1.1"
 DESCRIPTION = "Automatically find diff lines that need test coverage."
 QUALITY_DESCRIPTION = "Automatically find diff lines with quality violations."
