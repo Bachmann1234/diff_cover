e1b458f8705130202931c1d1026aac63bf990f3e
Matt Bachmann <bachmann.matt@gmail.com>
2021-08-07 11:04:03 -0400

    I dont actually _use_ tox

---
+++ verify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+black diff_cover tests
+isort diff_cover tests
+python -m pytest --cov --cov-report=xml tests
+git fetch origin main:refs/remotes/origin/main
+diff-cover coverage.xml --include-untracked
+diff-quality --violations flake8 --include-untracked
+diff-quality --violations pylint --include-untracked
+doc8 README.rst
