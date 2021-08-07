b77328b9a31c2cd056ce958594e20ef4b3083a9d
Matt Bachmann <bachmann.matt@gmail.com>
2021-08-07 10:54:46 -0400

    format... I really though I had this auto running

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,5 +1,5 @@
 import pkg_resources
 
-VERSION = pkg_resources.get_distribution('diff_cover').version
+VERSION = pkg_resources.get_distribution("diff_cover").version
 DESCRIPTION = "Automatically find diff lines that need test coverage."
 QUALITY_DESCRIPTION = "Automatically find diff lines with quality violations."
