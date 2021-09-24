af8020c754c5da3b8b34f5388fb9fb5308f4cd17
Matt Bachmann <bachmann.matt@gmail.com>
2021-09-24 18:23:28 -0400

    Add the version command to the verify script

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,12 +1,13 @@
 try:
-    from importlib.metadata import version  # noqa
-
-    VERSION = version("diff_cover")
+    from importlib.metadata import version
 except ImportError:
+    # Importlib.metadata introduced in python 3.8
     import pkg_resources
 
-    VERSION = pkg_resources.get_distribution("diff_cover").version
+    def version(package):
+        return pkg_resources.get_distribution(package).version
 
 
+VERSION = version("diff_cover")
 DESCRIPTION = "Automatically find diff lines that need test coverage."
 QUALITY_DESCRIPTION = "Automatically find diff lines with quality violations."
