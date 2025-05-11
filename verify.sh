987894f22736ab556d43eadefdd1aa0de653a8df
kingbuzzman <buzzi.javier@gmail.com>
2025-05-11 18:42:56 -0400

    .

--- verify.sh
+++ verify.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-black diff_cover tests 
-isort diff_cover tests 
+black diff_cover tests
+isort diff_cover tests
 python -m pytest -n auto --cov-context test --cov --cov-report=xml tests --cov-report=html --cov-report=term
 git fetch origin main:refs/remotes/origin/main
 diff-cover --version
