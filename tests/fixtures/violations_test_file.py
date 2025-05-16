686e18f0e6e75339d4e8fab853768ce3a2c6351a
kingbuzzman <buzzi.javier@gmail.com>
2025-05-16 04:18:02 -0400

    .

--- tests/fixtures/violations_test_file.py
+++ tests/fixtures/violations_test_file.py
@@ -1,14 +1,12 @@
 def func_1(apple, my_list):
-    if apple < 10:
-        # Do something
+    if apple<10:
+        # Do something 
         my_list.append(apple)
     return my_list[1:]
-
-
 def func_2(spongebob, squarepants):
     """A less messy function"""
     for char in spongebob:
         if char in squarepants:
             return char
-    unused = 1
+    unused=1
     return None
--- verify.sh
+++ verify.sh
@@ -3,13 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 ruff format --check .
-ruff check .
 python -m pytest -n auto --cov-context test --cov --cov-report=xml tests
 git fetch origin main:refs/remotes/origin/main
 diff-cover --version
 diff-quality --version
 diff-cover coverage.xml --include-untracked
-diff-quality --violations flake8 --include-untracked
-diff-quality --violations pylint --include-untracked
+diff-quality --violations ruff.check --include-untracked
 doc8 README.rst --ignore D001
 
