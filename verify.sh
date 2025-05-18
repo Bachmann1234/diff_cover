a0bf0e4f258a8812ce4c33f1473d29450a114c34
Matt Bachmann <Bachmann1234@users.noreply.github.com>
2025-05-18 14:54:40 -0400

    Revert "Giving help love (#473)" (#474)
    This reverts commit 02609affa2853e7a57bfad8b1fe67c6055275bec.

--- verify.sh
+++ verify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-COMPARE_BRANCH=${COMPARE_BRANCH:-origin/main}
 
 black diff_cover tests --check
 isort diff_cover tests --check
@@ -9,7 +8,8 @@ python -m pytest -n auto --cov-context test --cov --cov-report=xml tests
 git fetch origin main:refs/remotes/origin/main
 diff-cover --version
 diff-quality --version
-diff-cover coverage.xml --include-untracked --compare-branch=$COMPARE_BRANCH
-diff-quality --violations flake8 --include-untracked --compare-branch=$COMPARE_BRANCH
-diff-quality --violations pylint --include-untracked --compare-branch=$COMPARE_BRANCH
+diff-cover coverage.xml --include-untracked
+diff-quality --violations flake8 --include-untracked
+diff-quality --violations pylint --include-untracked
 doc8 README.rst --ignore D001
+
