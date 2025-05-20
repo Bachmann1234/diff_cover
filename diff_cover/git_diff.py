0f77d717dc562ac5bb7fefab97e5840225e61dec
Javier Buzzi <buzzi.javier@gmail.com>
2025-05-20 08:08:41 -0400

    Fixes bug in untracked cache (#478)
    * Fixes bug in untracked cache
    
    * Adds test
    
    * test rename

--- diff_cover/git_diff.py
+++ diff_cover/git_diff.py
@@ -112,9 +112,12 @@ class GitDiffTool:
             return self._untracked_cache
 
         output = execute(["git", "ls-files", "--exclude-standard", "--others"])[0]
-        if not output:
-            return []
-        return [to_unescaped_filename(line) for line in output.splitlines() if line]
+        self._untracked_cache = []
+        if output:
+            self._untracked_cache = [
+                to_unescaped_filename(line) for line in output.splitlines() if line
+            ]
+        return self._untracked_cache
 
 
 class GitDiffFileTool(GitDiffTool):
