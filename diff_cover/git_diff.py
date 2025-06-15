e070959be0f39ad61106f5bd2d57d4df64fbfcf5
Javier Buzzi <buzzi.javier@gmail.com>
2025-06-15 15:49:18 -0400

    Update git_diff.py

--- diff_cover/git_diff.py
+++ diff_cover/git_diff.py
@@ -132,7 +132,7 @@ class GitDiffFileTool(GitDiffTool):
         Raises a `GitDiffError` if the file cannot be read.
         """
         try:
-            with open(self.diff_file_path, encoding="utf-8") as file:
+            with open(self.diff_file_path, "r") as file:
                 return file.read()
         except OSError as e:
             error_message = (
